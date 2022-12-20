b9e1daff0d459898a228615d3c210f0122252423
pradeep4143 <vittapradeepreddy@gmail.com>
2022-12-20 13:45:28 +0530

    Updates

---
+++ precache-manifest.0a71e87df882966334f516d66452143e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1ed05eb9a2f806dd8d",
+    "url": "/todotlist/static/css/main.c899847a.chunk.css"
+  },
+  {
+    "revision": "3e1ed05eb9a2f806dd8d",
+    "url": "/todotlist/static/js/main.3e1ed05e.chunk.js"
+  },
+  {
+    "revision": "1743278c051df87abac6",
+    "url": "/todotlist/static/js/1.1743278c.chunk.js"
+  },
+  {
+    "revision": "88943d1e3a1bb62b2462",
+    "url": "/todotlist/static/js/2.88943d1e.chunk.js"
+  },
+  {
+    "revision": "0cd787dd7e8a7396bef0",
+    "url": "/todotlist/static/js/runtime~main.0cd787dd.js"
+  },
+  {
+    "revision": "99668af129a10afca827c48e6ab0a343",
+    "url": "/todotlist/index.html"
+  }
+];
